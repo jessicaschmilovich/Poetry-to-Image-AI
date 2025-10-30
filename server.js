138ee38c75dc7149fbf2957a312f79de99d838c6
Jessica Schmilovich <jessicaschmilovich@gmail.com>
2025-10-29 20:35:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const OpenAI = require('openai');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
